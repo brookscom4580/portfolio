305c9038f7af5926a8f17897cf13929a22aa1a5b
Greg Brooks <gbrooks@inkwebgroup.com>
2024-03-31 14:23:55 -0700

    more changes

--- app/containers/components/Contact.tsx
+++ app/containers/components/Contact.tsx
@@ -4,7 +4,7 @@ export default function Contact() {
       <h4>Contact</h4>
       <p>
         <a href="mailto:greg@brooksg.dev"><i>&#9993;</i> greg@brooksg.dev</a><br/>
-        <a href="tel:2068664072"><i>&#9742;</i> (206) 866-4072</a>
+        <a href="tel:6693384734"><i>&#9742;</i> (669) DEV-GREG</a>
       </p>
     </div>
   );
