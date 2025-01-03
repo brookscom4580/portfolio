c063aec0875acbe68513600f18f5de466a59f118
Greg Brooks <gbrooks@inkwebgroup.com>
2025-01-03 12:14:49 -0800

    fixes

--- app/containers/components/Intro.tsx
+++ app/containers/components/Intro.tsx
@@ -6,7 +6,7 @@ export default function Intro() {
         <h1>Greg Brooks</h1>
         <h2>Engineering Leader</h2>
         <p>
-          I am a Seattle based engineering leader, currently leading a team of full-stack engineers who work on various technical projects in the web/ad tech services space. I have over 12 years of experience in hands on engineering with tech such as JavaScript (Node/TS/React/Vue), PHP, HTML, CSS, Docker, and AWS. Additionally I have 5 years of combined experience as a tech lead/manager.I am a Seattle based engineering leader, currently leading a team of full-stack engineers who work on various technical projects in the web/ad tech services space. I have over 12 years of experience in hands on engineering with tech such as JavaScript (Node/TS/React/Vue), PHP, HTML, CSS, Docker, and AWS. Additionally I have 5 years of combined experience as a tech lead/manager.
+          I am a Seattle based engineering leader, currently leading a team of full-stack engineers who work on various technical projects in the web/ad tech services space. I have over 12 years of experience in hands on engineering with tech such as JavaScript (Node/TS/React/Vue), PHP, HTML, CSS, Docker, and AWS. Additionally I have 5 years of combined experience as a tech lead/manager.
         </p>
       </section>
     </div>
