43ff15132d4947b63dfa23ebfac6c7dc7dd510b8
Greg Brooks <gbrooks@inkwebgroup.com>
2025-01-03 12:10:29 -0800

    minor refresh

--- app/containers/components/Intro.tsx
+++ app/containers/components/Intro.tsx
@@ -1,25 +1,12 @@
 export default function Intro() {
   return (
     <div className="resume--intro panel">
-      <img src="/gregbrooks_color.jpg" alt="Greg Brooks" />
+      <img src="/323ab159-befa-4250-9604-7d4ff0b8cd8d.png" alt="Greg Brooks" />
       <section>
         <h1>Greg Brooks</h1>
-        <h2>Software Engineering Leader</h2>
+        <h2>Engineering Leader</h2>
         <p>
-          I lead teams of
-          full-stack engineers who work on various technical projects. I have
-          over 10 years of experience in web development, using technologies
-          such as JavaScript (Node/TS/React/Vue), HTML, CSS, Docker, and AWS.
-          <br/><br/>
-          My goals are to help my teams achieve engineering excellence, foster a
-          culture of learning and growth, and deliver high-quality solutions
-          that create value for our clients. I mentor, coach, and support my
-          engineers, while also contributing to the code base and the system
-          design and architecture. I collaborate with other engineering leaders,
-          product owners/managers, and stakeholders to ensure alignment and
-          coordination of our efforts. I am passionate about web development,
-          and I constantly seek to improve my skills and learn new technologies
-          and best practices.
+          I am a Seattle based engineering leader, currently leading a team of full-stack engineers who work on various technical projects in the web/ad tech services space. I have over 12 years of experience in hands on engineering with tech such as JavaScript (Node/TS/React/Vue), PHP, HTML, CSS, Docker, and AWS. Additionally I have 5 years of combined experience as a tech lead/manager.I am a Seattle based engineering leader, currently leading a team of full-stack engineers who work on various technical projects in the web/ad tech services space. I have over 12 years of experience in hands on engineering with tech such as JavaScript (Node/TS/React/Vue), PHP, HTML, CSS, Docker, and AWS. Additionally I have 5 years of combined experience as a tech lead/manager.
         </p>
       </section>
     </div>
